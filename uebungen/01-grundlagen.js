0c441d91ef6bcc16a3200a67e80b13e37d8444c1
David Pondes <156648228+pd156673@users.noreply.github.com>
2024-05-08 10:42:04 +0000

    grundlagen

--- uebungen/01-grundlagen.js
+++ uebungen/01-grundlagen.js
@@ -1,38 +1,83 @@
-console.log('GRUNDLAGEN');
-console.log('==========');
-
-console.log('Aufgabe 1');
-console.log('Erstelle zwei Variablen namens a und b. Initialisiere mit den Werten 1 und 2');
-
+console.log("Grundlagen");
+console.log("==========");
+ 
+console.log("Aufgabe 1");
+console.log("Erstelle zwei Variablen namens a und b. Initialisiere mit den Werten 1 und2");
+ 
+// Der Wert 1 wird zugewiesen (=) an eine Variable namens a.
 let a = 1;
 let b = 2;
-
-console.log('Der Wert der Variablen a ist: a + b');
-
-console.log('Aufgabe 2');
-console.log('Gib das Ergebnis der Addition von a und b aus');
-
+ 
+// MIt dem Plus-operator findet eine String-Verkettung statt. Ein String ist eine zeichenkette, eingerahmt mit hochkommas.
+console.log("Der wert der Variablen a ist:" +a);
+ 
+console.log("Aufgabe 2");
+console.log("Gib das Ergebnis der Addition von a und b aus.");
+ 
+// wenn links oder rechts vom Plusoperator ein Sttring steht wird verkettet.
+//Wenn links und rechts Zahlen stehen, wird addiert.
 console.log(a+b);
+console.log("Das ergebnis der addition:"+(a+b));
+ 
+console.log("Aufgabe 3");
+console.log("Gib das Ergebniss der Subtraktion, Multiplikation, Division von a und b aus.");
+ 
+console.log("Subtraktion:"+(a-b));
+console.log("Multiplikation:"+(a*b));
+console.log("Division:"+(a/b));
+ 
+console.log("Aufgabe 4");
+console.log("c ist das Ergebnis der Addition von a und b.");
+ 
+let c =a + b;
+console.log("Der Wert von c ist:" +c);
+ 
+ 
+ 
+ 
+ 
+console.log("Aufgabe5");
+console.log(" a und b sind die Seitenlängen der Katheten eines rechtwinkligen Dtreiecks. Bestimme die Länge der Hypotenuse c");
+ 
+// cQuadrat ist in Kamelhöcker-Notation geschrieben. Das heisst: zuerst ein Kleinbuschtabe. Verbundene Wörter beginnen groß.
+let cQuadrat= a*a+b*b;
+ 
+//Javascript kiennt eine Bibliothek namens Math in der Bibliothek gibt es eine Funktion namens sqrt. Als Parameter in den runden Klammern erwartet sqrt eine Zahl, aus der dann die Wurzel gezogen wird.
+c= Math.sqrt(cQuadrat);
+ 
+console.log("Die Hypotenuse ist" +c+" lang.");
+ 
+console.log('Aufgabe 6');
+console.log('Ein Kunde legt 100 Euro auf dem Sparbuch an. Jedes Jahr bekommt er 10% Zinsen. Wie viel bekommt der Kunde');
+console.log('nach zwei Jahren ausgezahlt. Beachte den Zinseszinseffekt.');
+ 
+let laufzeit = 2;
+let startkapital = 100;
+let zinssatz = 0.1;   /* Das Komma ist zur Entwicklungszeit ein Punkt. */
+ 
+let kapitalNachEinemJahr = startkapital * (1 + zinssatz);
+ 
+console.log("Kapital nach einem Jahr: " + kapitalNachEinemJahr + " EUR.");
+ 
+let kapitalNachZweiJahren = kapitalNachEinemJahr * (1 + zinssatz);
+ 
+console.log("Kapital nach zwei Jahren: " + kapitalNachZweiJahren + " EUR.");
+ 
+//let endkapital = Math.pow((startkapital * zinssatz) , laufzeit);
+ 
+let endkapital = startkapital * Math.pow(1 + zinssatz, laufzeit);
+ 
+console.log("Endkapital nach " + laufzeit + " Jahren: " + endkapital + " EUR.");
 
-console.log('Das Ergebnis der Addition:' + a + b)
+console.log('Aufgabe 7');
+console.log('Die Reihe aus der vorherigen aufgabe werden als Reihe dargestellt');
 
-console.log('Aufgabe 3');
-console.log('Gib das Ergebnis der Subtraktion, Multiplikattion, Division von a und b aus.');
+endkapital = startkapital;
+console.log ('endkapitalzubeginn');
+endkapital = startkapital * (1 + zinssatz);
+console.log('endkapitalNachEinemJahr');
+endkapital = startkapital * (1 + zinssatz);
+console.log('endkapitalNachZweiJahren');
 
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-
-console.log('Aufgabe 4');
-console.log('C ist das ergebnis der addtion von a und b');
-let c = a + b
-
-console.log("c hat den Wert: " + c);
-
-console.log('Aufgabe 5');
-console.log('a und b sind die Seitenlängen der Katheten eines Rechtwinkliges Dreiecks. Bestimme die Länge der Hypotenuse c');
-let cQuadrat = a * a + b * b; 
-
-c = Math.sqrt(cQuadrat);
-
-console.log('die Hypotenuse ist.' + c + ' lang')
+console.log('Aufgabe 8');
+console.log('In aufgabe 7 wurde die Anweisung endkapital = ... mehrfach wiederholt. Um sich tipparbeit zu sparen und die Wiederholung der Anweisung in der gewünschten H');
